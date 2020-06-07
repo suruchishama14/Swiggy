181339aa43bb3416c0a6954c88090c8e4d541ed6
suruchishama14 <63233825+suruchishama14@users.noreply.github.com>
2020-06-07 16:36:58 +0530

    error
    error

--- MySwiggyApplication/MySwiggyApplication/settings.py
+++ MySwiggyApplication/MySwiggyApplication/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'adminpage.apps.AdminpageConfig',
     'Restaurant.apps.RestaurantConfig',
     'Customer.apps.CustomerConfig',
-    'crispy_forms',
+
 
 
 
@@ -126,4 +126,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
--- MySwiggyApplication/Restaurant/forms.py
+++ MySwiggyApplication/Restaurant/forms.py
@@ -5,12 +5,8 @@ from adminpage.models import AreaModel, RestaurantTypeModel
 
 
 class RestaurantForm(forms.ModelForm):
-    restro_name = forms.CharField( max_length=30,label="")
-    restro_contact = forms.IntegerField(label="")
-    restro_email= forms.EmailField(max_length=100,label="")
-    restro_area = forms.ModelChoiceField(queryset=AreaModel.objects.all(),label="")
-    restro_type = forms.ModelChoiceField(queryset=RestaurantTypeModel.objects.all(),label="")
-    restro_password = forms.CharField(max_length=30,widget=forms.PasswordInput,label="")
+    restro_password = forms.CharField(max_length=30, widget=forms.PasswordInput)
+
     class Meta:
         model = RestaurantModel
         fields = "__all__"
